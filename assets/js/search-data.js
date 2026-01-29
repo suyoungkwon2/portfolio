e3e8584b23a23431f3232d9aba3e54a0b658edcc
suyoungkwon2 <suyoungkwon77@gmail.com>
2026-01-29 17:38:46 +0000

    Deploying to gh-pages from @ suyoungkwon2/portfolio@d5b3e7670de5ae919b5e576e9a191928f19ad4d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-suyoung-mel-kwon",
+    title: "Suyoung (Mel) Kwon",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
